3f0b95affdff366b8578a06e897146151315ba41
Supratim Mondal <supratimmondal50@gmail.com>
2020-08-11 21:28:37 +0530

    Fixing the problems

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,8 @@ import styles from "styled-components";
 // using styled components
 const Image = styles.img`
     max-width: 100%;
+    height: 110vh;
+    object-fit: cover;
 `;
 
 const Header = ({ imageName }) => {
--- src/components/yourAccount.js
+++ src/components/yourAccount.js
@@ -1,14 +1,20 @@
 import React from "react";
+import styles from "styled-components";
+
+const Section = styles.section`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
 
 const YourAccount = ({ imageName }) => {
   return (
-    <section style={{ paddingTop: 250, paddingLeft: 50 }}>
+    <Section style={{ paddingTop: 250 }}>
       <img
-        style={{ maxWidth: "60%" }}
+        style={{ maxWidth: "100%" }}
         src={require(`../images/${imageName}`)}
         alt="Image"
       />
-    </section>
+    </Section>
   );
 };
 
